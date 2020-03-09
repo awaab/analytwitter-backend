58266f86b49b3eb41bdc9a5cb6ee3310d599b13d
awaab <awaaab10@gmail.com>
2020-03-09 05:50:44 +0000

    changed search url

--- analytwitter_site/urls.py
+++ analytwitter_site/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('analytwitter/',include('analytwitter_app.urls', namespace='analytwitter_app')),
+    path('',include('analytwitter_app.urls', namespace='analytwitter_app')),
 ]
